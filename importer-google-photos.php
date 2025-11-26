62c7a027414cf90e747b1aef7bea98b4ff53e2bf
Fernando Fernandes <fernandofernand3s@gmail.com>
2025-11-25 22:20:26 -0300

    Bump version

--- importer-google-photos.php
+++ importer-google-photos.php
@@ -14,7 +14,7 @@
  * Plugin Name:             Album Importer for Google Photos
  * Plugin URI:              https://wpspecialprojects.wordpress.com
  * Description:             Import your Google Photos album images into your WordPress site and display them in a beautiful gallery.
- * Version:                 1.0.0-beta.4
+ * Version:                 1.0.0-beta.5
  * Requires at least:       6.7
  * Tested up to:            6.8
  * Requires PHP:            8.2
--- readme.txt
+++ readme.txt
@@ -4,7 +4,7 @@ Tags: google photos, album, gallery, importer, media library
 Requires at least: 6.7
 Tested up to: 6.8
 Requires PHP: 8.2
-Stable tag: 1.0.0-beta.4
+Stable tag: 1.0.0-beta.5
 License: GPLv3 or later
 License URI: http://www.gnu.org/licenses/gpl-3.0.html
 
